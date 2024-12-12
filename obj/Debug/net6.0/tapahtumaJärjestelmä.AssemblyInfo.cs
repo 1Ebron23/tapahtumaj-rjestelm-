bff21fee7120dd3106aba53ecb5f5ed900373a5c
saadq <fsaqa002@edu.xamk.fi>
2024-12-12 22:26:09 +0200

    TapahtumaJärjestelmä

--- obj/Debug/net6.0/tapahtumaJärjestelmä.AssemblyInfo.cs
+++ obj/Debug/net6.0/tapahtumaJärjestelmä.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tapahtumaJärjestelmä")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a02a2b2ea681603ee4aaab3eb60a84a9068f4d76")]
 [assembly: System.Reflection.AssemblyProductAttribute("tapahtumaJärjestelmä")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tapahtumaJärjestelmä")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
